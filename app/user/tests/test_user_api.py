6f4f4191c4a6cc034f0354be34d72aa639102fc9
Abhishek Mehta <abmaurya012@gmail.com>
2024-07-12 14:41:15 +0530

    test_user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -25,10 +25,10 @@ class PublicUserApiTests(TestCase):
 
     def test_create_user_success(self):
         """Test creating a user is successful."""
-        payload ={
+        payload = {
             'email':'test@example.com',
             'password': 'testpass123',
-            'name': 'Test Name'
+            'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -39,10 +39,10 @@ class PublicUserApiTests(TestCase):
 
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists"""
-        payload ={
+        payload = {
             'email':'test@example.com',
             'password': 'testpass123',
-            'name': 'Test Name'
+            'name': 'Test Name',
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
@@ -51,16 +51,16 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short_error(self):
         """Test an  error is returned if password is less than 5 charcters"""
-        payload ={
+        payload = {
             'email':'test@example.com',
             'password': 'pw',
-            'name': 'Test Name'
+            'name': 'Test Name',
         }
 
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code , status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
-            email = payload['email']
+            email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
